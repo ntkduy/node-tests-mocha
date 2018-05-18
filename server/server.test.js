c883c7ce9991dbf93e9e3b59c89c378dc09c360b
Tang Kha Duy NGUYEN <ntkduy1604@gmail.com>
2018-05-18 21:03:45 +0800

    Organising test with describe()

--- server/server.test.js
+++ server/server.test.js
@@ -3,25 +3,31 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Todo app v1.0'
+describe('Server', () => {
+
+  describe('GET /', () => {
+    it('should return hello world response', (done) => {
+      request(app)
+      .get('/')
+      .expect(404)
+      .expect((res) => {
+        expect(res.body).toInclude({
+          name: 'Todo app v1.0'
+        })
       })
-    })
-    .end(done);
-});
+      .end(done);
+    });
+  });
 
-
-it('should return \"Tang Nguyen\" in user list', (done) => {
-  request(app)
-    .get('/user')
-    // .expect(404)
-    .expect((res) => {
-      expect(res.body.userList).toInclude('Tang N guyen')
-    })
-    .end(done);
+  describe('GET /user', () => {
+    it('should return \"Tang Nguyen\" in user list', (done) => {
+      request(app)
+      .get('/user')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.userList).toInclude('Tang Nguyen')
+      })
+      .end(done);
+    });
+  });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,66 +1,55 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  var res = utils.add(33, 11);
-
-  expect(res).toBe(44).toBeA('number');
-  // if (res !== 44) {
-  //   throw new Error(`Expected 44, but got ${res}.`);
-  // }
-});
-
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
-  })
-})
-
-it('should square a number', () => {
-  var res = utils.square(2);
-
-  expect(res).toBe(4).toBeA('number');
-  // if (res !== 4) {
-  //   throw new Error(`Expected 4, but got ${res}.`);
-  // }
-});
-
-it('should async square a number', (done) => {
-  utils.asyncSquare(3, (square) => {
-    expect(square).toBe(9).toBeA('number');
-    done();
+describe('Utils', () => {
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      var res = utils.add(33, 11);
+      expect(res).toBe(44).toBeA('number');
+    });
   });
-})
 
-it('should expect some values', () => {
-  // expect(12).toNotBe(11);
-  // expect({name: 'Tang Nguyen'}).toEqual({name: 'Tang Nguyen'});
-  // expect([2, 3, 4]).toInclude(2);
-  expect({
-    name: 'Tang Nguye',
-    age: 25,
-    location: 'Australia'
-  }).toExclude({
-    age: 24
+  it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+      expect(sum).toBe(7).toBeA('number');
+      done();
+    })
   })
+
+  it('should square a number', () => {
+    var res = utils.square(2);
+    expect(res).toBe(4).toBeA('number');
+  });
+
+  it('should async square a number', (done) => {
+    utils.asyncSquare(3, (square) => {
+      expect(square).toBe(9).toBeA('number');
+      done();
+    });
+  });
+
+  it('should expect some values', () => {
+    // expect(12).toNotBe(11);
+    // expect({name: 'Tang Nguyen'}).toEqual({name: 'Tang Nguyen'});
+    // expect([2, 3, 4]).toInclude(2);
+    expect({
+      name: 'Tang Nguye',
+      age: 25,
+      location: 'Australia'
+    }).toExclude({
+      age: 24
+    })
+  });
 });
+
+
 
 it('should verify first and last names are set', () => {
   var user = {
     location: 'Western Australia',
     age: 31
   };
-
   var res = utils.setName(user, 'Duy NGUYEN');
-//
-  // expect(user).toEqual(res);
   expect(res.firstName).toExist().toBeA('string');
   expect(res.lastName).toExist().toBeA('string');
-
-
-  /*expect(res).toInclude({
-    firstName: 'Duy',
-    lastName: 'Nguyen'
-  });*/
 });
